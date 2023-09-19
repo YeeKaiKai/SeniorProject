125be05abab84356d1b92b44245f9e031f40b496
YeeKaiKai <kyle910204@gmail.com>
2023-09-19 23:59:38 +0800

    feat: divide createAndUpdate into create and update

---
+++ model/updateCart.js
@@ -0,0 +1,39 @@
+const getPool = require('./connectionDB.js');
+
+/**
+ * 新增顧客訂單，如果已經有了就會變修改數量。
+ * @param {*} customerID 
+ * @param {*} quantity 
+ * @param {*} food 
+ * @param {*} restaurantID 
+ * @param {*} restaurantName
+ */
+
+module.exports = async function (customerID, quantity, food, restaurantID, restaurantName) {
+
+    const pool = getPool(restaurantName);
+    for (let num = 0; num < quantity.length; num++) {
+        let sql =
+        `
+        UPDATE \`ORDER\` 
+        SET Amount = ${quantity[num]} 
+        WHERE Food = "${food[num]}"
+        AND CustomerID = "${customerID}" 
+        AND RestaurantID = "${restaurantID}"
+        `;
+        pool.getConnection((conn_err, connection) => {
+            if (conn_err) {
+                throw conn_err;
+            }
+            connection.query(sql, (query_err, results) => {
+                if (query_err) {
+                    throw query_err;
+                }
+                return results;
+            })
+            if (connection) {
+                connection.release();
+            }
+        })
+    }
+}
