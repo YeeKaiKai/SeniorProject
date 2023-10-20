092f9d527d2e1ed34f751a8aaccb1a3534a14b08
YeeKaiKai <kyle910204@gmail.com>
2023-10-20 22:21:36 +0800

    feat: add the model of delete order

---
+++ model/owner/deleteOrder.js
@@ -0,0 +1,27 @@
+const getPool = require('../connectionDB.js');
+
+module.exports = async function (customerID, restaurantID, restaurantName) {
+
+    return new Promise((resolve, reject) => {
+        let sql = 
+        `
+        DELETE FROM \`ORDER\`
+        WHERE CustomerID = "${customerID}" AND RestaurantID = "${restaurantID}" AND Confirmed = TRUE;
+        `;
+        const pool = getPool(restaurantName);
+        pool.getConnection((conn_err, connection) => {
+            if (conn_err) {
+                throw conn_err;
+            }
+            connection.query(sql, (query_err, results) => {
+                if (query_err) {
+                    throw query_err;
+                }
+                resolve(results);
+            })
+            if (connection) {
+                connection.release();
+            }
+        })
+    }) 
+}
