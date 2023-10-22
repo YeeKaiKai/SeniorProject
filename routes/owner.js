4f5e8b5d5a5064d504b68d848488bbbbffe4c67e
YeeKaiKai <kyle910204@gmail.com>
2023-10-23 01:22:26 +0800

    feat: add routes of custom

--- routes/owner.js
+++ routes/owner.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const order = require('../controller/order.js')
+const custom = require('../controller/custom.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,17 @@ router.get('/', function(req, res, next) {
 
 router.get('/order', order.getOrder);
 router.delete('/order', order.deleteOrder);
+
+router.post('/custom', custom.postCustom);
+router.post('/custom/option', custom.postOption);
+
+router.delete('/custom', custom.deleteCustom);
+router.delete('/custom/option', custom.deleteOption);
+
+router.get('/custom', custom.getCustom);
+router.get('/custom/option', custom.getOption);
+
+router.put('/custom', custom.putCustom);
+router.put('/custom/option', custom.putOption);
 
 module.exports = router;
