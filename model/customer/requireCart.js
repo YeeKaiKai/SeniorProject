2dcb6521d29ab6024f40f21e65c55eb8cb307d95
Kai Yee <kyle910204@gmail.com>
2023-10-30 11:34:38 +0800

    feat: modify query to get cart and cart_customize

--- model/customer/requireCart.js
+++ model/customer/requireCart.js
@@ -5,16 +5,21 @@ module.exports = async function (restaurantName, customerID) {
     return new Promise((resolve, reject) => {
         let sql = 
         `
-        SELECT Food, Amount, Note 
-        FROM \`CART\`
-        WHERE Confirmed = FALSE AND RestaurantName = "${restaurantName}" AND CustomerID = "${customerID}"
+        SELECT * 
+        FROM CART AS C
+        RIGHT JOIN CART_CUSTOMIZE AS CC
+        ON C.CustomerID = CC.CustomerID
+            AND C.Food = CC.Food
+            AND C.CustomID = CC.CustomID
+            AND C.RestaurantName = CC.RestaurantName
+        WHERE Confirmed = FALSE AND RestaurantName = ? AND CustomerID = ?
         `;
         const pool = getPool();
         pool.getConnection((conn_err, connection) => {
             if (conn_err) {
                 reject(conn_err);
             } 
-            connection.query(sql, (query_err, results) => {
+            connection.query(sql, [restaurantName, customerID], (query_err, results) => {
                 if (query_err) {
                     reject(query_err);
                 } 
