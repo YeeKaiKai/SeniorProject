d9be6b786048353085e0c32f4604f56fd81288e6
Kai Yee <kyle910204@gmail.com>
2023-11-06 15:53:54 +0800

    feat: add connection tool for model

---
+++ model/connectionTool.js
@@ -0,0 +1,65 @@
+exports.getConnection = async function (pool) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((conn_err, connection) => {
+            if (conn_err) {
+                return reject(conn_err);
+            }
+            resolve(connection);
+        });
+    });
+}
+
+exports.beginTransaction = async function (connection) {
+    return new Promise((resolve, reject) => {
+        connection.beginTransaction((tran_err) => {
+            if (tran_err) {
+                return reject(tran_err);
+            }
+            resolve();
+        });
+    });
+}
+
+exports.query = async function (connection, sql, params) {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, params, (query_err, results) => {
+            if (query_err) {
+                return reject(query_err);
+            }
+            resolve(results);
+        });
+    });
+}
+
+exports.rollback = async function (connection) {
+    return new Promise((resolve, reject) => {
+        connection.rollback((roll_err) => {
+            if (roll_err) {
+                return reject(roll_err);
+            }
+            resolve();
+        });
+    })
+}
+
+exports.commit = async function (connection) {
+    return new Promise((resolve, reject) => {
+        connection.commit((commit_err) => {
+            if (commit_err) {
+                return reject(commit_err);
+            }
+            resolve();
+        });
+    })
+}
+
+exports.release = async function (connection) {
+    return new Promise((resolve, reject) => {
+        connection.release((release_err) => {
+            if (release_err) {
+                return reject(release_err);
+            }
+            resolve();
+        });
+    })
+}
