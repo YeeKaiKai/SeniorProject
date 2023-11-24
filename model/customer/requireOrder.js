b8e0e69be4fe9992f45879c9ab69d23c3c938716
YeeKaiKai <kyle910204@gmail.com>
2023-11-25 01:53:19 +0800

    fix: modify the customer get order sql that the method should beequal to owner get order

--- model/customer/requireOrder.js
+++ model/customer/requireOrder.js
@@ -5,16 +5,22 @@ module.exports = async function (restaurantName, customerID) {
 
     let sql = 
     `
-    SELECT *
+    SELECT c.CustomerID, c.Amount, c.Food, c.CustomID, c.RestaurantName, c.Note, c.Confirmed, 
+            c.OrderID, cc.Option, cc.Custom, o.TotalSum, o.orderNote, o.OrderDate, o.OrderTime, o.Forhere, o.TableNumber, o.PhoneNumber, o.Paid
     FROM CART c
-    LEFT JOIN CART_CUSTOMIZE cc 
-    ON c.CustomerID = cc.CustomerID 
+    LEFT JOIN CART_CUSTOMIZE cc ON c.CustomerID = cc.CustomerID 
     AND c.Food = cc.Food 
     AND c.CustomID = cc.CustomID 
     AND c.RestaurantName = cc.RestaurantName
-    INNER JOIN \`ORDER\` o 
-    ON c.OrderID = o.OrderID 
-    WHERE c.RestaurantName = ? AND c.CustomerID = ?
+    LEFT JOIN CUSTOM_OPTION co ON cc.\`Option\` = co.\`Option\` 
+    AND cc.Custom = co.Custom 
+    AND cc.RestaurantName = co.RestaurantName
+    INNER JOIN MENU m ON c.Food = m.Food 
+    AND c.RestaurantName = m.RestaurantName
+    INNER JOIN \`ORDER\` o ON c.OrderID = o.OrderID 
+    AND c.RestaurantName = o.RestaurantName
+    WHERE c.restaurantName = ? AND c.Confirmed = TRUE AND c.CustomerID = ?
+    ORDER BY o.OrderID, c.Food, cc.custom ASC
     `;
     const pool = getPool();
     const connection = await connectionTool.getConnection(pool);
