6615b93380736768409126ba2ed0a5631b170af8
YeeKaiKai <kyle910204@gmail.com>
2023-12-04 20:30:57 +0800

    feat: move get menu from index to customer

--- routes/customer.js
+++ routes/customer.js
@@ -5,6 +5,7 @@ const chatgpt = require('../controller/customer/chatgpt.js');
 const cart = require('../controller/customer/cart.js')
 const custom = require('../controller/customer/custom.js')
 const order = require('../controller/customer/order.js')
+const menu = require('../controller/customer/menu.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,18 +13,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/:restaurantName/chat', chatgpt.postDiagolue);
-router.get('/:restaurantName/chat', chatgpt.getDialogue);
-
-router.post('/:restaurantName/cart', cart.postCart);
-router.delete('/:restaurantName/cart', cart.deleteCart);
-router.get('/:restaurantName/cart', cart.getCart);
-router.patch('/:restaurantName/cart', cart.patchCart);
-
 router.post('/:restaurantName/order', order.postCartToOrder);
-router.get('/:restaurantName/order', order.getOrder);
+router.post('/:restaurantName/cart', cart.postCart);
 
+router.delete('/:restaurantName/cart', cart.deleteCart);
+
+router.get('/:restaurantName/chat', chatgpt.getDialogue);
+router.get('/:restaurantName/order', order.getOrder);
+router.get('/:restaurantName/cart', cart.getCart);
+router.get('/:restaurantName/menu', menu.getMenu);
 router.get('/:restaurantName/:food/customize', custom.getCustomizeOption);
 
+router.patch('/:restaurantName/cart', cart.patchCart);
 
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 const index = require('../controller/index.js')
-const menu = require('../controller/customer/menu.js');
 
 router.post('/regist', index.postRegistRestaurant);
 router.post('/login', index.postLoginRestaurant);
 
 router.get('/', index.getCustomerID)
 router.get('/restaurant', index.getRestaurant)
-router.get('/:restaurantName/menu', menu.getMenu);
 router.get('/:restaurantName/category', menu.getCategory);
 
 module.exports = router;
