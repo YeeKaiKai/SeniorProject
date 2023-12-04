57cae77df756edfa2957a6fa7d7e97357fe64435
YeeKaiKai <kyle910204@gmail.com>
2023-12-04 23:08:24 +0800

    feat: add old and new food, category

--- controller/owner/menu.js
+++ controller/owner/menu.js
@@ -25,16 +25,18 @@ exports.postMenu = async function(req, res, next) {
 exports.patchMenu = async function(req, res, next) {
 
     try {
-        let food = req.body.food;
+        let oldFood = req.body.oldFood;
+        let newFood = req.body.newFood;
         let description = req.body.description;
         let defaultQuantity = req.body.defaultQuantity;
         let quantity = req.body.quantity;
         let ingredient = req.body.ingredient;
         let price = req.body.price;
-        let category = req.body.category;
+        let oldCategory = req.body.oldCategory;
+        let newCategory = req.body.newCategory;
         let custom = req.body.custom;
         let restaurantName = req.params.restaurantName;
-        await updateMenu(food, description, defaultQuantity, quantity, ingredient, price, category, custom, restaurantName);
+        await updateMenu(oldFood, newFood, description, defaultQuantity, quantity, ingredient, price, oldCategory, newCategory, custom, restaurantName);
         res.status(204).send();
     } catch (error) {
         res.status(500).send(error);
--- model/owner/menu/updateMenu.js
+++ model/owner/menu/updateMenu.js
@@ -1,14 +1,14 @@
 const getPool = require('../../connectionDB.js');
 const connectionTool = require('../../connectionTool.js');
 
-module.exports = async function (food, description, defaultQuantity, quantity, ingredient, price, category, custom, restaurantName) {
+module.exports = async function (oldFood, newFood, description, defaultQuantity, quantity, ingredient, price, oldCategory, newCategory, custom, restaurantName) {
 
     const pool = getPool();
     const connection = await connectionTool.getConnection(pool);
     let updateMenuSql = 
     `
     UPDATE MENU
-    SET Food = ?, Description = ?, DefaultQuantity = ?, Quantity = ?, Ingredient = ?, Price = ?
+    SET Food = ?, Description = ?, DefaultQuantity = ?, Quantity = ?, Ingredient = ?, Category = ?, Price = ?
     WHERE Food = ? AND Category = ? AND RestaurantName = ?
     `;
     let deleteCustomSql = 
@@ -23,11 +23,11 @@ module.exports = async function (food, description, defaultQuantity, quantity, i
     `;
     try {
         connectionTool.beginTransaction(connection);
-        await connectionTool.query(connection, updateMenuSql, [food, description, defaultQuantity, quantity, ingredient, price, food, category, restaurantName]);
-        await connectionTool.query(connection, deleteCustomSql, [food, category, restaurantName]);
+        await connectionTool.query(connection, updateMenuSql, [newFood, description, defaultQuantity, quantity, ingredient, newCategory, price, oldFood, oldCategory, restaurantName]);
+        await connectionTool.query(connection, deleteCustomSql, [oldFood, oldCategory, restaurantName]);
         if (custom != null) {
             for (let num = 0; num < custom.length; num++) {
-                await connectionTool.query(connection, customToFoodSql, [custom[num], food, category, restaurantName]);
+                await connectionTool.query(connection, customToFoodSql, [custom[num], newFood, newCategory, restaurantName]);
             }
         }
         connectionTool.commit(connection);
